0313a044068f24392650ee41b56c1312304ab390
ridia13 <ridia2005@naver.com>
2021-07-20 17:03:17 +0900

    on click card

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 "use strict";
+
 const total = 12;
 const colors = ['red', 'orange', 'yellow', 'green', 'white', 'pink'];
 const colorCopy = colors.concat(colors);
+const $msg = document.querySelector('#js-msg');
 let shuffled = [];
+let clicked = [];
+let completed = [];
 
 function shuffle() { //피셔-예이츠 셔플
   for (let i = 0; colorCopy.length > 0; i++) {
     const randomIndex = Math.floor(Math.random() * colorCopy.length);
     shuffled = shuffled.concat(colorCopy.splice(randomIndex, 1));
   }
-
 }
 
 function createCard(i) { //.card > .card-inner> (.card-front + .card-back)
-  // for (let i = 0; i < total; i++) {
   const card = document.createElement('div');
   card.className = 'card';
   const cardInner = document.createElement('div');
@@ -27,7 +29,6 @@ function createCard(i) { //.card > .card-inner> (.card-front + .card-back)
   cardInner.appendChild(cardBack);
   card.appendChild(cardInner);
   return card;
-  // }
 }
 
 function startGame() {
@@ -35,6 +36,7 @@ function startGame() {
   for (let i = 0; i < total; i++) {
     const $wrapper = document.querySelector('#wrapper');
     const card = createCard(i);
+    card.addEventListener('click', onClickCard);
     $wrapper.appendChild(card);
   }
 
@@ -50,8 +52,34 @@ function startGame() {
   })
 }
 
-startGame();
+function onClickCard() {
+  this.classList.toggle('flipped');
+  clicked.push(this);
 
-function init() {}
+  if (clicked.length != 2) {
+    return;
+  }
+  const firstBackColor = clicked[0].querySelector('.card-back').style.backgroundColor;
+  const secondBackColor = clicked[1].querySelector('.card-back').style.backgroundColor;
 
+  if (firstBackColor !== secondBackColor) {
+    setTimeout(() => {
+      clicked[0].classList.remove('flipped');
+      clicked[1].classList.remove('flipped');
+      clicked = [];
+    }, 500);
+    return;
+  }
+  completed = completed.concat(clicked);
+  clicked = [];
+
+  if (completed.length !== total) {
+    return;
+  }
+  $msg.textContent = "Congratulations!!";
+}
+
+function init() {
+  startGame();
+}
 init();
